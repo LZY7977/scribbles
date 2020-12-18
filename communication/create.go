0e9bfd58c02deeb43e60df689de4c24edb0ceac9
Yannan Zhu <zyannan@amazon.com>
2020-12-18 06:26:38 -0800

    trigger

--- communication/create.go
+++ communication/create.go
@@ -26,7 +26,7 @@ func createDefaultLobbyCreatePageData() *CreatePageData {
 	return &CreatePageData{
 		SettingBounds:     game.LobbySettingBounds,
 		Languages:         game.SupportedLanguages,
-		DrawingTime:       "99",
+		DrawingTime:       "100",
 		Rounds:            "4",
 		MaxPlayers:        "12",
 		CustomWordsChance: "50",
