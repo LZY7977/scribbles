afa41a43aa2829b6cbdf54ed60afba1554ba20a1
Yannan Zhu <zyannan@amazon.com>
2020-12-17 17:44:37 -0800

    update default drawing time to 88 to see whether we can see change

--- communication/create.go
+++ communication/create.go
@@ -26,7 +26,7 @@ func createDefaultLobbyCreatePageData() *CreatePageData {
 	return &CreatePageData{
 		SettingBounds:     game.LobbySettingBounds,
 		Languages:         game.SupportedLanguages,
-		DrawingTime:       "77",
+		DrawingTime:       "88",
 		Rounds:            "4",
 		MaxPlayers:        "12",
 		CustomWordsChance: "50",
